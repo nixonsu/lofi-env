9e68545c48a74442700bdd0505d75d3832c1850d
ni-xon <nixonuch_@outlook.com>
2022-03-06 15:50:07 +1100

    feat: add routes middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes";
+
+// Load environment variables
+dotenv.config();
+const port = process.env.PORT;
+
+// Instantiate express app
+const app = express();
+
+// Routes middleware
+app.use("/api/users", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
