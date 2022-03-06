918fbd5928b35922ad4e436631c045e9d8140ae9
ni-xon <nixonuch_@outlook.com>
2022-03-07 10:16:54 +1100

    feat: add schemas for array elements

--- backend/models/user.model.ts
+++ backend/models/user.model.ts
@@ -1,11 +1,27 @@
 import mongoose from "mongoose";
 
+const youtubeLinksArraySchema = new mongoose.Schema({
+  link: {
+    type: String,
+  },
+});
+
+const tasksArraySchema = new mongoose.Schema({
+  task: { type: String },
+});
+
 // Define schema for user collection
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    name: { type: String, requried: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    name: { type: String, requried: [true, "Please add a name"] },
+    password: { type: String, required: [true, "Please add a password"] },
+    tasksArray: { type: [tasksArraySchema], required: false },
+    youtubeLinksArray: { type: [youtubeLinksArraySchema], required: false },
   },
   { timestamps: true }
 );
