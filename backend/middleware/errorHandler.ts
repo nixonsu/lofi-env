6811438a1690c471015e330211fe182afc677d26
ni-xon <nixonuch_@outlook.com>
2022-03-06 16:36:09 +1100

    feat: add error handler middleware

---
+++ backend/middleware/errorHandler.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // If there is no status code, default to 500 Internal Server Error
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "prod" ? null : err.stack,
+  });
+};
+
+export default errorHandler;
