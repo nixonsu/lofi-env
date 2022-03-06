6df7c3b59ff04b6941a88a022b265fe4d84d66ce
ni-xon <nixonuch_@outlook.com>
2022-03-06 16:36:38 +1100

    feat: add error handler middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes";
+import errorHandler from "./middleware/errorHandler";
 
 // Load environment variables
 dotenv.config();
 const port = process.env.PORT;
 
-// Instantiate express app
+// Instantiate express app and use built-in middleware
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes middleware
 app.use("/api/users", userRoutes);
+
+// Custom error handler middleware ()
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
