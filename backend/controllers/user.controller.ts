eb18c9cf36f787140f60621468943436b6a33a6f
ni-xon <nixonuch_@outlook.com>
2022-03-06 17:06:02 +1100

    feat: add express async handler to enable use of custom error middleware and async functions

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -1,31 +1,36 @@
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 
 // @desc    Get users
 // @route   GET /api/users
 // @access  Private
-const getUsers = (req: Request, res: Response) => {
+const getUsers = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: "Get users" });
-};
+});
 
 // @desc    Create user
 // @route   POST /api/users
 // @access  Private
-const createUser = (req: Request, res: Response) => {
+const createUser = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("Please add a name field");
+  }
   res.status(200).json({ message: "Create user" });
-};
+});
 
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private
-const updateUser = (req: Request, res: Response) => {
+const updateUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Update user ${req.params.id}` });
-};
+});
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private
-const deleteUser = (req: Request, res: Response) => {
+const deleteUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Delete user ${req.params.id}` });
-};
+});
 
 export default { getUsers, createUser, updateUser, deleteUser };
