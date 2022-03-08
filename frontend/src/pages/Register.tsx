06f7b63562e87558f5185e636e7e9c61b3c45cb8
ni-xon <nixonuch_@outlook.com>
2022-03-08 11:21:51 +1100

    feat: add functionality to register form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { SyntheticEvent } from "react";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { name, email, password, confirmPassword } = formData;
 
-export default Register
+  const handleOnChange = (e: SyntheticEvent) => {
+    const element = e.target as HTMLInputElement;
+    setFormData((prevState) => ({
+      ...prevState,
+      [element.name]: element.value,
+    }));
+  };
+
+  const handleOnSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter Name"
+            onChange={handleOnSubmit}
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter Email"
+            onChange={handleOnChange}
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Passowrd"
+            onChange={handleOnChange}
+          />
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirm Password"
+            onChange={handleOnChange}
+          />
+          <button type="submit"> Submit</button>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Register;
