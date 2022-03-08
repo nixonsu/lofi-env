a5f161d04642b6e4028936d2e107a029c1b51767
ni-xon <nixonuch_@outlook.com>
2022-03-08 14:37:43 +1100

    feat: add toastify to react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Nav from "./components/Nav";
@@ -15,6 +17,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
