317577612698eef383c97302b04e58c3d092fc1a
ni-xon <nixonuch_@outlook.com>
2022-03-08 11:27:21 +1100

    feat: add login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { SyntheticEvent } from "react";
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
 
-export default Login
+  const handleOnChange = (e: SyntheticEvent) => {
+    const element = e.target as HTMLInputElement;
+    setFormData((prevState) => ({
+      ...prevState,
+      [element.name]: element.value,
+    }));
+  };
+
+  const handleOnSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />
+          Login
+        </h1>
+        <p>Login to your account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter Email"
+            onChange={handleOnChange}
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Passowrd"
+            onChange={handleOnChange}
+          />
+          <button type="submit"> Login</button>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
