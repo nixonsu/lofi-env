5850e18999e62ee1ff44a0a9ab00c9b7fddca5b9
ni-xon <nixonuch_@outlook.com>
2022-03-18 13:27:47 +1100

    feat: complete prepare deployment

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import userRoutes from "./routes/user.routes";
 import linkRoutes from "./routes/link.routes";
 import taskRoutes from "./routes/task.routes";
@@ -21,6 +22,24 @@ app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/links", linkRoutes);
 app.use("/api/colors", colorRoutes);
+
+console.log(process.env.NODE_ENV);
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  // Path for static assets (build)
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  // Serve index.html
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) =>
+    res.send("Please set to environment variables to production")
+  );
+}
 
 // Custom error handler middleware ()
 app.use(errorHandler);
