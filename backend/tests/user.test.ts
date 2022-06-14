df54d4edbbfbb6ce6f200450bd4ab1350e6da6dd
ni-xon <nixonuch_@outlook.com>
2022-06-14 13:49:27 +1000

    test: add skeleton tests for user resource

---
+++ backend/tests/user.test.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+describe("User", () => {
+  describe("Login user", () => {
+    describe("Given user exists", () => {});
+    describe("Given user does not exist", () => {});
+  });
+
+  describe("Create user", () => {
+    describe("Given user exists", () => {});
+    describe("Given user does not exist", () => {});
+  });
+
+  describe("Get user", () => {
+    describe("Given user is logged in", () => {});
+    describe("Given user is not logged in", () => {});
+  });
+
+  describe("Update user", () => {
+    describe("Given user is logged in", () => {});
+    describe("Given user is not logged in", () => {});
+  });
+
+  describe("Delete user", () => {
+    describe("Given user is logged in", () => {});
+    describe("Given user is not logged in", () => {});
+  });
+});
