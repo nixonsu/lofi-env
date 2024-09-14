98b29747f598a86ef6e0e516275b81736c09c41a
ni-xon <nixon.s.uch@gmail.com>
2024-09-15 00:43:51 +1000

    fix: add missing env variable

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -19,5 +19,6 @@ flyctl secrets set \
        DB_URI="$DB_URI" \
        PORT="$PORT" \
        JWT_SECRET="$JWT_SECRET" \
+       ENV="$ENV" \
        -a "lofi-env"
 flyctl deploy --config "./ops/deployment/fly.toml" -a "lofi-env" --image "$REGISTRY_NAME/$LOCAL_IMAGE_NAME:$IMAGE_SUFFIX" --remote-only
