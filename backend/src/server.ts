af26ab11c5e0df8342bcb794bb54c1952621141c
ni-xon <nixon.s.uch@gmail.com>
2024-09-15 12:35:18 +1000

    chore: add default port 4000 for backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ let port: string;
 if (process.env.PORT) {
   port = process.env.PORT;
 } else {
-  port = "80"; // Default to port 80
+  port = "4000"; // Default to port 4000
 }
 
 // Instantiate express app and use built-in middleware
